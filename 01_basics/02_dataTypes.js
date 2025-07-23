69f7fd4da0e9cd34ce7c74661db8d20aafc9abc7
kripanshbaghel <kripanshbghl@gmail.com>
2025-07-23 15:06:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treats all JS codes as newer version
+
+//alert( 3 + 3) // we are using NodeJS, not browser
+
+console.log(3
+     + 3); // code readability should be high
+
+console.log("Kripansh");
+
+let name = "Kripansh"
+let age = 99
+let IsLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
+
+
+
+
+

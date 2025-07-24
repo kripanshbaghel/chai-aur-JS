87384a3333f317b3cfbac155da0c7c3e7ccf9e75
kripanshbaghel <kripanshbghl@gmail.com>
2025-07-24 15:36:04 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedin = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "Kripansh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
